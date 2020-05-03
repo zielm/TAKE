9362b08f06a92e07593ea8ea1f99f728d8f68b1e
zielm <zielinska0323@gmail.com>
2020-05-03 21:25:12 +0200

    Spring

---
+++ Spring/bookings/src/main/java/lab/bookings/repositories/BookingRepository.java
@@ -0,0 +1,8 @@
+package lab.bookings.repositories;
+
+import lab.bookings.models.Booking;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookingRepository
+        extends CrudRepository<Booking, Long> {
+}
