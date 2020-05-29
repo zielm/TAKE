5ba8f3824676266f8f4624e0bb6b042ff01e1963
zielm <zielinska0323@gmail.com>
2020-05-30 00:17:48 +0200

    ASP.NET Web API, Ajax

---
+++ WebApiAjax/WebApiAjax/Models/CalcResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAjax.Models
+{
+    public class CalcResult
+    {
+        public int Sum { get; set; }
+        public int Difference { get; set; }
+        public int Product { get; set; }
+        public int Quotient { get; set; }
+    }
+
+}
