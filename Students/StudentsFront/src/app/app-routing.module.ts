6a1eaee261a8474015147a08def7fae65d9421eb
zielm <zielinska0323@gmail.com>
2020-06-05 00:22:20 +0200

    Aplikacja SPA: Angular + .NET Core Web API

---
+++ Students/StudentsFront/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { AboutComponent } from './about/about.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent },
+  { path: 'detail/:id', component: StudentDetailComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Students/StudentsFront/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentService } from './student.service';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentsComponent,
+    StudentDetailComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [StudentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
